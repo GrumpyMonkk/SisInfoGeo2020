6319cfd399f65dd106973f1b28532949637db259
GrumpyMonkk <60480345+GrumpyMonkk@users.noreply.github.com>
2020-02-13 12:19:20 -0600

    Add files via upload

---
+++ 14.CuentaBancaria/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _14.CuentaBancaria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CuentaBancaria evers = new CuentaBancaria(1000);
+            Console.WriteLine($"Saldo al crear {evers.Saldo}");
+            evers.Saldo=500;
+            Console.WriteLine($"Saldo despues del domingo {evers.Saldo}");
+            if(evers.Retira(2500))
+            Console.WriteLine($"Saldo despues de la peda {evers.Saldo}");
+            else
+            Console.WriteLine($"No tienes pito");
+        }
+    }
+}
